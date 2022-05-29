15b912694807c5727222abd0e1f500ff8213e2af
Chandra <210101011@mhs.udb.ac.id>
2022-05-29 11:31:51 +0700

    materi input data

---
+++ 5.Input Data User/5.inputdatauser.py
@@ -0,0 +1,14 @@
+#data input pasti str
+data = input("Masukan data : ")
+print("Data = ",data,"type = ",type(data))
+
+#data integer/float
+data_int = int(input("Masukan angka : "))
+print("Data = ",data_int,"type = ",type(data_int))
+
+data_float = float(input("Masukan angka : "))
+print("Data = ",data_float,"type = ",type(data_float))
+
+#boolean
+data_bool = bool(int(input("Masukan data : ")))
+print("Data = ",data_bool,"type = ",type(data_bool))
