859291873fc69fb778204034b0280bdc2d34589a
Chandra <210101011@mhs.udb.ac.id>
2022-05-29 20:39:38 +0700

    materi if dan else

---
+++ 18.If and Else Statement/18.ifelse_statement.py
@@ -0,0 +1,28 @@
+# If dan Else Statement
+#1.If nya
+#2.Kondisinya
+#3.Aksinya
+
+nama = input('Siapa nama anda : ')
+
+#program sebelumnya
+#if kondisi:aksi
+#program selanjutnya
+
+#1.ini adalah program if inline
+#if nama =='chandra': print('Kamu gans abies')
+#print(f'terimakasih {nama}')
+
+#2.program if indentasi/indentation
+#if nama == 'chandra':
+#    print('Kamu gans abies')
+#    print('kamu juga keren baget')
+
+#print(f'trimakasih {nama}')
+
+#.else statement
+if nama =='otong':
+    print('Hi tong')
+else:
+    print('LO bukan otong')
+print('akhir dari program')
