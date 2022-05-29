653e31bc34ccd35762a53d701f2fec38f91eb53d
Chandra <210101011@mhs.udb.ac.id>
2022-05-29 11:30:58 +0700

    materi tipe data

---
+++ 3.Tipe Data/3.tipedata.py
@@ -0,0 +1,28 @@
+#tipedata : angka satuan : integer
+data_integer = 1
+print("data : ",data_integer)
+print("-bertipe : ", type(data_integer))
+
+#tipe data float(angka dengan koma)
+data_float = 1.5
+print("data : ",data_float)
+print("-bertipe :",type(data_float))
+
+#tipe data string (tulisan)
+data_string = "chandra"
+print("data : ",data_string)
+print("-bertipe :",type(data_string))
+
+#tipe data biner true/false (boolean)
+data_bool = True
+print("data : ",data_bool)
+print("-bertie : ",type(data_bool))
+
+##tipe data khusus
+
+#data komplek
+data_complex = complex(5,6)
+print("data : ",data_complex)
+print("-bertie : ",type(data_complex))
+
+#tipe data bahasa c
