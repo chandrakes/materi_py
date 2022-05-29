be1384e43c5b7a97671f76bde7bd4a6b800644b8
Chandra <210101011@mhs.udb.ac.id>
2022-05-29 11:32:37 +0700

    materi operasi aritmatika

---
+++ 6.Operasi Arit Matika/6.operasiaritmatika.py
@@ -0,0 +1,42 @@
+'''
+urutan operasi/prioritas :
+1.()
+2.eksponen
+3.kali bagi eksponen modulus floordiv
+4.tambahkurang
+'''
+#tambah +
+a = 10
+b = 3
+hasil = a + b
+print(a,'+',b,'=',hasil)
+#kurang - 
+a = 10
+b = 3
+hasil = a - b
+print(a,'-',b,'=',hasil)
+#Kali *
+a = 10
+b = 3
+hasil = a * b
+print(a,'x',b,'=',hasil)
+#bagi /
+a = 10
+b = 3
+hasil = a / b
+print(a,':',b,'=',hasil)
+#eksponen/pangkat **
+a = 10
+b = 3
+hasil = a ** b
+print(a,'**',b,'=',hasil)
+#modulus/hasilbagi %
+a = 10
+b = 3
+hasil = a % b
+print(a,'%',b,'=',hasil)
+#floor division // dibulatkan kebawah
+a = 10
+b = 3
+hasil = a // b
+print(a,'//',b,'=',hasil)
