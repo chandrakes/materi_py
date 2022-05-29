c8511f62c4f40526a61e7795c4c82e281e806a72
Chandra <210101011@mhs.udb.ac.id>
2022-05-29 11:30:36 +0700

    materi variable

---
+++ 2.Variable/2.variable.py
@@ -0,0 +1,21 @@
+
+#Variable adalah tempat menyimpan data
+#assigment nilai
+a = 10
+x = 5
+panjang = 1000
+#pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+#penamaan
+nilai_y = 15 #menggunakan underscore
+juta10 = 10000000 #nomor tidak boleh didepan
+nilaiZ = 17.5 #boleh
+#pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+#assigment indirect
+b = a
+print("Nilai b = ", b)
